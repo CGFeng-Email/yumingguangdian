ada3a7a030c1cc0b8dbf63ab575673a7fd98b9e0
cgf <1291933022@qq.com>
2023-05-23 21:24:28 +0800

    更新;2C

--- js/commin.js
+++ js/commin.js
@@ -8,10 +8,17 @@ $('#navbar .nav .nav_li').hover(function () {
     $(this).find('.iconfont').removeClass('active');
 })
 
-// 下拉导航栏 产品图切换
+// pc 下拉导航栏 产品图切换
 $('.menuDown .left_content .list-group-item').hover(function () {
    let coverImg = $(this).attr('data-cover');
    let coverText = $(this).attr('data-text')
    $('.menuDown .right_cover .cover').attr('src', coverImg);
    $('.menuDown .right_cover .cover').text(coverText);
+})
+
+// h5 下拉导航栏 展开
+$('#navbar .nav .nav_li_wrap .icon').click(function () {
+    $(this).toggleClass('icon_rolate');
+    $(this).parent('.nav_li_wrap').siblings('.h5_menuDown').slideToggle('slow');
+
 })
